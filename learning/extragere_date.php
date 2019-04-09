3e6e91e858a1f806e028914732fee30681fb150b
StefanDaniel23 <danezu_2006@yahoo.com>
2019-04-09 13:22:34 +0300

    modificari

--- learning/extragere_date.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('config.php');
-// Selectare dare din baza de date
-$cerereSQL = 'SELECT * FROM `formular`';
-$rezultat = mysql_query($cerereSQL);
- while($rand = mysql_fetch_array($rezultat)) {
- echo $rand['nume'];
- }
-?> 
--- learning/formular.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo 'Nume: '.$_POST['nume'].' <br>
- Prenume: '.$_POST['prenume'].' <br>
- Varsta: '.$_POST['varsta'].' <br>
- Parola: '.$_POST['parola'].' <br>'; 
